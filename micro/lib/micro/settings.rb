c5a235de0fcb4c5ad2ef059cbad4af2a0c8ee686
Matthew M. Boedicker <matthewm@boedicker.org>
2012-09-04 10:15:30 -0700

    Update settings for new MCF components.
    Change-Id: I3010241d2fc712d7dfad50e8ef4e7391dee4a40e

--- micro/lib/micro/settings.rb
+++ micro/lib/micro/settings.rb
@@ -22,12 +22,30 @@ module VCAP
       def self.randomize_passwords(properties)
         randomize_password(properties, 'cc', 'token', 64)
         randomize_password(properties, 'cc', 'password', 64)
+        randomize_password(properties, 'cc', 'staging_upload_user', 8)
+        randomize_password(properties, 'cc', 'staging_upload_password', 9)
         randomize_password(properties, 'router', 'password', 8)
         router = properties['router']
         randomize_password(router, 'status', 'user', 8)
         randomize_password(router, 'status', 'password', 8)
         randomize_password(properties, 'nats', 'password', 8)
-        randomize_password(properties, 'ccdb', 'password', 8)
+
+        ccdb_password = secret(8)
+        properties['ccdb']['roles'].find {
+          |role| role['tag'] == 'admin' }['password'] = ccdb_password
+
+        acmdb_password = secret(8)
+        properties['ccdb']['roles'].find {
+          |role| role['tag'] == 'acm' }['password'] = acmdb_password
+        properties['acmdb']['roles'].find {
+          |role| role['tag'] == 'admin' }['password'] = acmdb_password
+
+        uaadb_password = secret(8)
+        properties['ccdb']['roles'].find {
+          |role| role['tag'] == 'uaa' }['password'] = uaadb_password
+        properties['uaadb']['roles'].find {
+          |role| role['tag'] == 'admin' }['password'] = uaadb_password
+
         randomize_password(properties, 'mysql_node', 'password', 8)
         randomize_password(properties, 'mysql_gateway', 'token', 4)
         randomize_password(properties, 'redis_gateway', 'token', 4)
@@ -39,9 +57,24 @@ module VCAP
         properties['postgresql_node'] = {} unless properties['postgresql_node']
         properties['postgresql_node']['admin_passwd_hash'] = properties['postgresql_gateway']['admin_passwd_hash']
 
-        randomize_password(properties, 'rabbitmq_srs', 'admin_passwd_hash', 4)
-        randomize_password(properties, 'rabbitmq_srs', 'token', 64)
-        randomize_password(properties, 'rabbitmq_srs', 'backbone_password', 4)
+        randomize_password(properties, 'rabbit_gateway', 'token', 64)
+
+        randomize_password(properties, 'acm', 'user', 16)
+        randomize_password(properties, 'acm', 'password', 16)
+
+        randomize_password(properties, 'serialization_data_server',
+          'upload_token', 32)
+
+        vcap_redis_password = secret(24)
+        properties['service_lifecycle']['resque']['password'] = \
+          vcap_redis_password
+        properties['vcap_redis']['password'] = vcap_redis_password
+
+        randomize_password(properties['uaa'], 'cc', 'token_secret', 32)
+        randomize_password(properties['uaa'], 'cc', 'client_secret', 32)
+        randomize_password(properties['uaa'], 'admin', 'client_secret', 24)
+        randomize_password(properties['uaa'], 'batch', 'username', 13)
+        randomize_password(properties['uaa'], 'batch', 'password', 13)
 
         properties
       end
